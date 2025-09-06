ebc9d770c294cfa5507ee744507ae42d963e5615
More Samarth Nagesh <115178193+moresamarth2020@users.noreply.github.com>
2025-09-06 14:11:09 +0530

    Add files via upload

---
+++ Day_70.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Exercise-9
+# Write a program to pronounce list of names using win32 API. 
+# If you are given a list l as follows:
+# ```python
+# l = ["Rahul", "Nishant", "Harry"]
+# ```
+# Your program should pronouce:
+# ```
+# Shoutout to Rahul
+# Shoutout to Nishant
+# Shoutout to Harry
+# ```
+
+# In[ ]:
+
+
+
+
